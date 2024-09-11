4e09ff3bf1195597a06345ece7f9edaa4dd28339
benedhk <frrdhika@gmail.com>
2024-09-11 15:37:26 +0700

    day8 commit

--- src/Day7/AverageCalculator.java
+++ src/Day7/AverageCalculator.java
@@ -2,8 +2,6 @@ package Day7;
 
 import java.util.Scanner;
 
-import java.util.Scanner;
-
 public class AverageCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
