f3ca4d7f2d7fe1f4c11a7798cb0ca6d1ba983c2a
benedhk <frrdhika@gmail.com>
2024-09-17 10:13:21 +0700

    day10 commit

---
+++ src/Day10/Magazine.java
@@ -0,0 +1,24 @@
+package Day10;
+public class Magazine extends LibraryMaterial {
+    private String publisher;
+    private int issueNumber;
+
+    public Magazine(String title, int availabilityCount, String publisher, int issueNumber) {
+        super(title, availabilityCount, "Magazine");
+        this.publisher = publisher;
+        this.issueNumber = issueNumber;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public int getIssueNumber() {
+        return issueNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Magazine: " + super.toString() + ", Publisher: " + publisher + ", Issue Number: " + issueNumber;
+    }
+}
